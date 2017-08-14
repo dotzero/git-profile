8f722e7c52d0d272f4178f63c654c5963a9f791b
dotzero <mail@dotzero.ru>
2017-08-14 23:24:44 +0300

    Update del cmd

--- cmd/del.go
+++ cmd/del.go
@@ -7,13 +7,15 @@ import (
 )
 
 var delCmd = &cobra.Command{
-	Use:     "del [profile]",
-	Aliases: []string{"d"},
-	Short:   "Delete an entry from profile",
-	Long: `Delete an entry from profile.
+	Use:     "del [profile] [key]",
+	Aliases: []string{"rm"},
+	Short:   "Delete an entry or profile",
+	Long: `Delete an entry from profile or an entire profile.
+Provide a "key" argument to remove only that key from profile.
 
 Example:
-  git-profile del work`,
+  git-profile del my-profile -> to delete an entire profile
+  git-profile del my-profile user.name -> to delete only user.name`,
 	Run: delRun,
 }
 
@@ -29,6 +31,13 @@ func delRun(cmd *cobra.Command, args []string) {
 
 	profile := args[0]
 
-	сfgStorage.RemoveProfile(profile)
+	if len(args) == 2 {
+		cmd.Print("Delete an entry from profile:\n\n")
+		сfgStorage.RemoveValue(profile, args[1])
+	} else {
+		cmd.Print("Delete an entire profile:\n\n")
+		сfgStorage.RemoveProfile(profile)
+	}
+
 	сfgStorage.Save(cfgFile)
 }
