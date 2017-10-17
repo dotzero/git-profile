ce3755df3956622366d68cd54194d6623e74f1b7
dotzero <mail@dotzero.ru>
2017-10-17 21:40:50 +0300

    A little code cleanup

--- cmd/current.go
+++ cmd/current.go
@@ -3,15 +3,18 @@ package cmd
 import (
 	"os"
 
-	"github.com/spf13/cobra"
 	"github.com/dotzero/git-profile/git"
+	"github.com/spf13/cobra"
 )
+
+// DefaultProfileName is a default profile name if not selected
+const DefaultProfileName = `default`
 
 var currentCmd = &cobra.Command{
 	Use:     "current",
 	Aliases: []string{"c"},
-	Short:   "Show current profile",
-	Long:    "Show current profile for repo",
+	Short:   "Show selected profile",
+	Long:    "Show selected profile for current repository.",
 	Run:     currentRun,
 }
 
@@ -21,22 +24,22 @@ func init() {
 
 func currentRun(cmd *cobra.Command, args []string) {
 	if len(cfgStorage.Profiles) == 0 {
-		cmd.Print(`no profiles found`)
+		cmd.Print(`There are no available profiles.
+To add a profile, use the following examples:
+  git-profile add my-profile user.name "John Doe"
+  git-profile add my-profile user.email work@example.com`)
 		os.Exit(0)
 	}
 
-	if (!git.IsRepository()) {
-		//cmd.Print(`this is not a repository`)
+	if !git.IsRepository() {
 		os.Exit(1)
 	}
 
 	res, err := git.GetLocalConfig(`current-profile.name`)
-
-	if (len(res) == 0 || err != nil){
-		cmd.Print(`default`)
+	if len(res) == 0 || err != nil {
+		cmd.Print(DefaultProfileName)
 		os.Exit(0)
 	}
 
 	cmd.Printf("%s", res)
-	os.Exit(0)
 }
--- git/git.go
+++ git/git.go
@@ -24,9 +24,8 @@ func SetLocalConfig(key string, value string) error {
 	return nil
 }
 
-// GetLocalConfig set git local config key with value
+// GetLocalConfig get git local config value by key
 func GetLocalConfig(key string) ([]byte, error) {
 	log.Printf("[DEBUG] git config --local %s \n", key)
-	res, err := exec.Command("git", "config", "--local", key).Output();
-	return res, err
+	return exec.Command("git", "config", "--local", key).Output()
 }
